a91109aec7c866c64dff9f5dab83744f8af2e11e
MilagrosLv <lopezvmilagros@gmail.com>
2025-09-05 16:54:25 -0300

    TP1 ej2, electrodomesticos terminado

--- TP1/src/electrodomesticos/Main.java
+++ TP1/src/electrodomesticos/Main.java
@@ -3,8 +3,20 @@ package electrodomesticos;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Electrodomestico e1 = new Electrodomestico("Lavarropas");
+		Electrodomestico e2 = new Electrodomestico("Heladera", "Plateado", 5000);
+		Electrodomestico e3 = new Electrodomestico("Microondas", "Blanco", 200, 70, 5);
+		
+		//e1
+		System.out.println(e1.getNombre() + ": " + e1.esBajoConsumo() + ", " + e1.balanceCostoPeso() + ", " + e1.esAltaGama());
+		
+		//e2
+		System.out.println(e2.getNombre() + ": " + e2.esBajoConsumo() + ", " + e2.balanceCostoPeso() + ", " + e2.esAltaGama());
+		
+		//e3
+		System.out.println(e3.getNombre() + ": " + e3.esBajoConsumo() + ", " + e3.balanceCostoPeso() + ", " + e3.esAltaGama());
+		
+		
 	}
 
 }
