5d3bca4b5e3faa1ee2a4ba46cc056e4009e68942
MilagrosLv <lopezvmilagros@gmail.com>
2025-09-06 13:54:45 -0300

    TP2 ej2 Reunion actualizada

--- TP2/src/agendaPersonal/Reunion.java
+++ TP2/src/agendaPersonal/Reunion.java
@@ -11,11 +11,11 @@ public class Reunion {
 	
 	
 	public Reunion(String lugar, String tema, LocalTime duracion, LocalDateTime fechaHoraInicio) {
-		this.lugar = lugar;
-		this.tema = tema;
-		this.duracion = duracion;
-		this.fechaHoraInicio = fechaHoraInicio;
-		this.fechaHoraFin = ;
+		setLugar(lugar);
+		setTema(tema);
+		setDuracion(duracion);
+		setFechaHoraInicio(fechaHoraInicio);
+		this.fechaHoraFin = crearFechaHoraFin(duracion, fechaHoraInicio);
 		this.participantes = new ArrayList<>();
 	}
 
@@ -47,9 +47,32 @@ public class Reunion {
 	public LocalDateTime getFechaHoraFin() {
 		return fechaHoraFin;
 	}
+
 	
 	//OTHER METHODS
-	
+	public LocalDateTime crearFechaHoraFin(LocalTime duracion, LocalDateTime fechaHoraInicio) {
+		return fechaHoraInicio.plusHours(duracion.getHour()).plusMinutes(duracion.getMinute());
+	}
+	public void agregarPersona(Persona p) {
+		int cantPersonaRepetida = 0;
+		for(Persona pp : participantes) {
+			if(p.equals(pp)) {
+				System.out.println("Este participante ya se anotó");
+				cantPersonaRepetida++;
+			}
+		}
+		if(cantPersonaRepetida < 0) {
+			System.out.println("No se pudo agregar al participante");
+		} else {
+			participantes.add(p);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Reunion [lugar=" + lugar + ", tema=" + tema + ", duracion=" + duracion + ", fechaHoraInicio="
+				+ fechaHoraInicio + ", fechaHoraFin=" + fechaHoraFin + ", participantes=" + participantes + "]";
+	}
 
 	
 	
