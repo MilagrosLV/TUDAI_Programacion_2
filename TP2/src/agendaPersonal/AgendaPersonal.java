6b9052986e0981714de77cf8bf6ac7cda2e39f60
MilagrosLv <lopezvmilagros@gmail.com>
2025-09-06 17:21:57 -0300

    TP2 EJ2 Agenda Personal actualizada

--- TP2/src/agendaPersonal/AgendaPersonal.java
+++ TP2/src/agendaPersonal/AgendaPersonal.java
@@ -6,4 +6,42 @@ public class AgendaPersonal {
 	private Persona usuario;
 	private ArrayList<Reunion> reuniones;
 	
+	
+	public AgendaPersonal(Persona usuario) {
+		this.usuario = usuario;
+		this.reuniones = new ArrayList<>();
+	}
+
+	//GETTERS AND SETTERS METHODS
+	public Persona getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Persona usuario) {
+		this.usuario = usuario;
+	}
+	public ArrayList<Reunion> getReuniones() {
+		return reuniones;
+	}
+	
+	//OTHER METHODS
+	public void agregarReunion(Reunion r) {
+		if(!hayConflicto(r)) {
+			reuniones.add(r);
+		} else {
+			System.out.println(r.getTema()+" se superpone con otro evento. No puede agregarse a la agenda");
+		}
+	}
+	public boolean hayConflicto(Reunion r){
+		for(Reunion rr : reuniones) {
+			if(seSuperpone()) {
+				
+			}
+		}
+	}
+	public boolean seSuperpone() {
+		
+	}
+	
+	
+	
 }
