cc09078291b1a18c581d06b22d0359a781af5ca4
MilagrosLv <lopezvmilagros@gmail.com>
2025-09-08 14:15:53 -0300

    TP2 EJ2 AgendaPersonal terminada

--- TP2/src/agendaPersonal/AgendaPersonal.java
+++ TP2/src/agendaPersonal/AgendaPersonal.java
@@ -25,21 +25,25 @@ public class AgendaPersonal {
 	
 	//OTHER METHODS
 	public void agregarReunion(Reunion r) {
-		if(!hayConflicto(r)) {
+		if(!hayConflictoFecha(r)) {
 			reuniones.add(r);
 		} else {
 			System.out.println(r.getTema()+" se superpone con otro evento. No puede agregarse a la agenda");
 		}
 	}
-	public boolean hayConflicto(Reunion r){
+	public boolean hayConflictoFecha(Reunion r){
 		for(Reunion rr : reuniones) {
-			if(seSuperpone()) {
-				
+			if(r.getFechaHoraInicio().getYear() == rr.getFechaHoraInicio().getYear() && r.getFechaHoraInicio().getMonthValue() == rr.getFechaHoraInicio().getMonthValue() && r.getFechaHoraInicio().getDayOfMonth() == rr.getFechaHoraInicio().getDayOfMonth()) {
+				//Misma fecha --> comparo año, mes y día
+				if(!seSuperponeHorario(r, rr)) { //Conflicto de hora en el mismo día
+					return true;
+				}
 			}
 		}
+		return false;
 	}
-	public boolean seSuperpone() {
-		
+	public boolean seSuperponeHorario(Reunion r, Reunion rr) {
+		return (r.getFechaHoraInicio().isAfter(rr.getFechaHoraFin()) || r.getFechaHoraFin().isBefore(rr.getFechaHoraInicio()));
 	}
 	
 	
