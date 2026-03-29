c45dcf95df832c50d54ffb002e8c15ebbd5a8f8d
MilagrosLv <lopezvmilagros@gmail.com>
2026-03-29 03:48:51 -0300

    TP2 EJ4

--- TP2/src/establecimientoDeportivo1/Establecimiento.java
+++ TP2/src/establecimientoDeportivo1/Establecimiento.java
@@ -3,14 +3,16 @@ package establecimientoDeportivo1;
 import java.util.ArrayList;
 
 public class Establecimiento {
+	//atributos
 	private ArrayList<Usuario> usuarios;
 	private ArrayList<Cancha> canchas;
-	private ArrayList<Turno>agenda;
+	private ArrayList<Turno> turnos;
 	
+	//Constructor
 	public Establecimiento() {
 		this.usuarios = new ArrayList<Usuario>();
 		this.canchas = new ArrayList<Cancha>();
-		this.agenda = new ArrayList<Turno>();
+		this.turnos = new ArrayList<Turno>();
 	}
 
 	
@@ -18,14 +20,56 @@ public class Establecimiento {
 	public ArrayList<Usuario> getUsuarios() {
 		return usuarios;
 	}
-	public ArrayList<Cancha> getCanchas() {
+	public ArrayList<Cancha> getCancha() {
 		return canchas;
 	}
 	public ArrayList<Turno> getAgenda() {
-		return agenda;
+		return turnos;
 	}
 	
 	//OTROS MÉTODOS
+	public void addUsuario(Usuario u) {
+		if(!getUsuarios().contains(u)) {
+			getUsuarios().add(u);
+		} else {
+			System.out.println(u+" ya existe en la lista");
+		}
+	}
+	public void addCancha(Cancha u) {
+		if(!getCancha().contains(u)) {
+			getCancha().add(u);
+		} else {
+			System.out.println(u+" ya existe en la lista");
+		}
+	}
+	public void addTurno(Turno u) {
+		if(!getAgenda().contains(u)) {
+			if(!isConflict(u)) {
+				getAgenda().add(u);
+			}
+		} else {
+			System.out.println(u+" ya existe en la lista");
+		}
+	}
 	
+	private int contarCanchasOcupadas(Turno t) {
+		int contador = 0;
+		for(Turno tt : turnos) {
+			if(t.getC().equals(tt.getC())) {
+				
+			}
+		}
+		return contador;
+	}
+
+
+	private boolean isConflict(Turno t) {
+		for(Turno tt : turnos) {
+			if(tt.getFechaHoraI().isBefore(t.getFechaHoraI()) && ) {
+				
+			}
+		}
+		return false;
+	}
 	
 }
